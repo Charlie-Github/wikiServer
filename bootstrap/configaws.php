4961fc00d3cd632282889362cd968039126b6bcf
Charlie <gcte2010@gmail.com>
2014-06-24 11:19:08 -0500

    addphp fetch s3 image phpfile

---
+++ bootstrap/configaws.php
@@ -0,0 +1,14 @@
+<?php return array(
+    'includes' => array('_aws'),
+    'services' => array(
+        'default_settings' => array(
+            'params' => array(
+                'key'    => 'AKIAJ3MRYFLTZCBUBEXA',
+                'secret' => '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',
+                // OR: 'profile' => 'my_profile',
+                'region' => 'us-west-2'
+            )
+        )
+    )
+);
+?>
